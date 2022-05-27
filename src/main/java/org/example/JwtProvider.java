cf64f5bac3d3c4772a08935e79fd4024b9500497
tis <92219795+this-is-spear@users.noreply.github.com>
2022-05-27 23:56:33 +0900

    feat : 유효시간 추가

--- src/main/java/org/example/JwtProvider.java
+++ src/main/java/org/example/JwtProvider.java
@@ -4,30 +4,33 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 
-import java.util.Map;
+import java.util.Date;
 import java.util.stream.Collectors;
 
 public class JwtProvider {
     private static final String ISSUER = "auth0";
     private static final String SECRET_KEY = "secret";
     private static final Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-    private static final JWTVerifier verifier = JWT.require(algorithm).withIssuer(ISSUER).build();
+    private static final JWTVerifier verifier = JWT.require(algorithm).withIssuer(ISSUER).acceptLeeway(1).acceptExpiresAt(5).build();
     private static final String AUTHORITIES_KEY = "Auth";
     public static final String DELIMITER = ",";
+    private final long tokenValidityInMilliseconds = 5 * 1000;
 
     public String encode(Authentication authentication) {
         String authorities = getAuthorities(authentication);
         try {
+            long now = (new Date()).getTime();
+            Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
             String token = JWT.create()
                 .withSubject((String) authentication.getPrincipal())
                 .withClaim(AUTHORITIES_KEY, authorities)
                 .withIssuer(ISSUER)
+                .withExpiresAt(validity)
                 .sign(algorithm);
-            System.out.println(token);
             return token;
         } catch (JWTCreationException exception) {
             return "";
@@ -41,10 +44,8 @@ public class JwtProvider {
         return authorities;
     }
 
-    public Map<String, Claim> decode(String token) {
-        DecodedJWT jwt = verifier.verify(token);
-        Map<String, Claim> claims = jwt.getClaims();
-        return claims;
+    public DecodedJWT decode(String token) {
+        return verifier.verify(token);
     }
 
     public boolean verify(String token) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,25 @@
 package org.example;
 
 import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Map;
 
 public class Main {
     private final static JwtProvider jwtProvider = new JwtProvider();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Authentication authentication = new FakeAuthentication();
         String token = jwtProvider.encode(authentication);
 
         if (jwtProvider.verify(token)) {
-            Map<String, Claim> decode = jwtProvider.decode(token);
+            DecodedJWT jwt = jwtProvider.decode(token);
+            Map<String, Claim> decode = jwt.getClaims();
             for (Map.Entry<String, Claim> stringClaimEntry : decode.entrySet()) {
                 System.out.println(stringClaimEntry.getKey() + " : " + stringClaimEntry.getValue());
             }
+
+            System.out.println("jwt getExpiresAt = " + jwt.getExpiresAt());
         }
     }
 }
